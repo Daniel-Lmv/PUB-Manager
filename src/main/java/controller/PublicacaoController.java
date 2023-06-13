4e660f4d36f817be7c867fa4a8186298281ed88f
Daniel Limaverde <daniellmvsilva@gmail.com>
2023-06-13 17:36:48 -0300

    Algumas atualizações e organização

--- src/main/java/controller/PublicacaoController.java
+++ src/main/java/controller/PublicacaoController.java
@@ -1,15 +1,23 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.result.InsertOneResult;
 
+import com.sun.tools.javac.util.List;
 import model.Publicacao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/pub-manager")
 public class PublicacaoController {
 	private MongoCollection<Publicacao> _publicacoes;
+	private final ArrayList<Publicacao> ListOsPubs = new ArrayList<Publicacao>();
 	
 	/**
 	 * Construtor da classe
@@ -18,14 +26,40 @@ public class PublicacaoController {
 	public PublicacaoController() throws Exception
 	{
 		//Chama a função que cria a conexão com o banco
-		_publicacoes = controller.Connection.getDatabase().getCollection("Publicacoes", Publicacao.class);	
+		_publicacoes = controller.Connection.getDatabase().getCollection("Publicacoes", Publicacao.class);
 	}
-	
+
 	/**
-	 * Método que adiciona uma nova publicação ao banco
+	 * Método getAllUserPubs, retorna todas as publicações de um determinado usuário
+	 */
+	@GetMapping
+	public ArrayList<Publicacao> getAllUserPubs()
+	{
+		return ListOfPubs;
+		/*MongoCursor cursor = _publicacoes.find().iterator();
+		try {
+			while(cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+		} finally {
+			cursor.close();
+		}*/
+	}
+
+	public void getOpen()
+	{}
+
+	public Publicacao getLastPub()
+	{
+		return null;
+	}
+
+
+	/**
+	 * Método postPub, adiciona uma nova publicação ao banco
 	 * @param publicacao
 	 */
-	public void addPublicacao(Publicacao publicacao)
+	public void postPub(Publicacao publicacao)
 	{
 		//Cria uma coleção
 		InsertOneResult result = _publicacoes.insertOne(publicacao);
@@ -38,18 +72,15 @@ public class PublicacaoController {
 		}
 	}
 
+
 	/**
-	 * Método consultaPublicação, pesquisa uma pub no banco pelo titulo da pub
+	 * Método putPub, atualiza uma publicação já existente
 	 */
-	public void consultaPublicacao()
+	public void putPub()
 	{
-		MongoCursor cursor = _publicacoes.find().iterator();
-		try {
-            while(cursor.hasNext()) {
-                System.out.println(cursor.next());
-            }
-       } finally {
-             cursor.close();
-        }
+
 	}
+
+	public void delPub()
+	{}
 }
